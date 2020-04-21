029ccc0d18a441e1b80d2bca121ebb0ce7214cae
Pjotrs Osnovskis <pjotrs.osnovskis@gmail.com>
2020-04-21 19:09:01 +0100

    message controller, hit messages to the db

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+  #
+  # def logged_in?
+  #   !!current_user
+  # end
 
 end
