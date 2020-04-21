d0be0cbdf899a99bc5b031633f42961e38d29805
Pjotrs Osnovskis <pjotrs.osnovskis@gmail.com>
2020-04-21 16:31:15 +0100

    display actual messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_190625) do
+ActiveRecord::Schema.define(version: 2020_04_20_200445) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
